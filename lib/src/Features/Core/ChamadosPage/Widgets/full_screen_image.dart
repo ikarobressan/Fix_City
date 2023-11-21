cc02c7e3072a2813e259d53fdf233a8602d7865f
Ikaro Bressan <zikarobressan@gmail.com>
2023-11-21 20:38:24 -0300

    removed unsed imports

--- lib/src/Features/Core/ChamadosPage/Widgets/full_screen_image.dart
+++ lib/src/Features/Core/ChamadosPage/Widgets/full_screen_image.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 import '../../../../Constants/colors.dart';
 
--- lib/src/Features/Core/ChamadosPage/Widgets/full_screen_video_player.dart
+++ lib/src/Features/Core/ChamadosPage/Widgets/full_screen_video_player.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:video_player/video_player.dart';
 
 class FullScreenVideoPlayer extends StatefulWidget {
